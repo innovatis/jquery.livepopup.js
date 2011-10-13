18ba63e34fd97dd4b67023af2ed9e480e28e8e6e
Tim Herd <erauqssidlroweht@eqdw.net>
2011-10-13 16:50:41 -0500

    wrap ajax in exo-popup

--- jquery.livepopup.js
+++ jquery.livepopup.js
@@ -1,57 +1,59 @@
 (function($){
 
-  function postHandler(data, button){      
+  function postHandler(data, button){
       // var popupContent = data.html;
       var popupContent = "<p>Text echoed back to request</p>",
           popup        = button.data("popupElement"),
           shouldCacheAjax = button.is('[data-cache-ajax]');
 
         if(! popup){
-           popup = $(popupContent).hide().appendTo("body");
-           button.data( "popupElement", popup ); 
+           popup = $("<div class='exo-popup'" + popupContent + "</div>").hide().appendTo("body");
+           button.data( "popupElement", popup );
         }
         togglePopup(popup, button);
     }
-    
+
     function togglePopup(popup, button){
       var closesOnClick = button.is("[data-close-on-click]");
         if( button.data('popup') !== 'initialized'){
         button.data('popup', 'initialized');
 
        if(closesOnClick){
-          popup.click( function(){ 
+          popup.click( function(){
             popup.toggle();
           });
         }
       }
       popup.toggle();
     }
-    
-    
+
+
   function actsAsPopup(e){
     e.preventDefault();
     var button    = $(e.target),
         href      = button.attr("href"),
-        element   = button.data('popupElement'), 
+        element   = button.data('popupElement'),
         isAjax    = (href[0] !== "#"),
         ajaxIsntCached     = !element,
         ajaxIsCacheable    = button.is("[data-cache-ajax]"),
-        shouldMakeAjaxCall = (ajaxIsntCached || !ajaxIsCacheable); 
+        shouldMakeAjaxCall = (ajaxIsntCached || !ajaxIsCacheable);
 
     if(! href){
       throw("[actsAsPopup]: href is empty");
     }
-      
+
     if(isAjax && shouldMakeAjaxCall ){
-      $.post(href, {html: "<p>test</p>"}, function(data){
-        postHandler(data, button);
-      });
+      //there is no ajax server in testing
+      //$.post(href, {html: "<p>test</p>"}, function(data){
+      //  postHandler(data, button);
+      //});
+      postHandler("jakhdfjkads", button);
     } else {
       var popup = element || $(href);
       togglePopup(popup, button);
     }
   }
-  
+
   $.fn.exoPopup = function(){
     this.live('click', actsAsPopup);
   };
