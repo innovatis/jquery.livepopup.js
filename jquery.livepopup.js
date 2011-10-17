83243d11bcf9b9dd4b5fa199f18f4ade9ea6a623
Tim Herd <erauqssidlroweht@eqdw.net>
2011-10-17 10:12:02 -0500

    works with proper ajax now

--- jquery.livepopup.js
+++ jquery.livepopup.js
@@ -1,53 +1,54 @@
 (function($){
 
   function postHandler(data, button){
-      // var popupContent = data.html;
-      var popupContent = "<p>Text echoed back to request</p>",
-          popup        = button.data("popupElement"),
-          shouldCacheAjax = button.is('[data-cache-ajax]');
+    var popupContent = data,
+      popup           = button.data("popupElement"),
+      shouldCacheAjax = button.is('[data-cache-ajax]');
 
-        if(! popup){
-           popup = $("<div class='exo-popup'" + popupContent + "</div>").hide().appendTo("body");
-           button.data( "popupElement", popup );
-        }
-        togglePopup(popup, button);
+    if(! popup){
+      popup = $("<div class='exo-popup'>" + popupContent + "</div>").hide();
+      popup.insertAfter(button);
+      button.data( "popupElement", popup );
     }
+    togglePopup(popup, button);
+  }
 
-    function togglePopup(popup, button){
-      var closesOnClick = button.is("[data-close-on-click]");
-        if( button.data('popup') !== 'initialized'){
-        button.data('popup', 'initialized');
+  function togglePopup(popup, button){
+    var closesOnClick = button.is("[data-close-on-click]");
+    if( button.data('popup') !== 'initialized'){
+      button.data('popup', 'initialized');
 
-       if(closesOnClick){
-          popup.click( function(){
-            popup.toggle();
-          });
-        }
+      button.exoPositionRelative(popup, {top: 20});
+
+
+      if(closesOnClick){
+        popup.click( function(){
+          popup.toggle();
+        });
       }
-      popup.toggle();
     }
+    popup.toggle();
+  }
 
 
   function actsAsPopup(e){
     e.preventDefault();
     var button    = $(e.target),
-        href      = button.attr("href"),
-        element   = button.data('popupElement'),
-        isAjax    = (href[0] !== "#"),
-        ajaxIsntCached     = !element,
-        ajaxIsCacheable    = button.is("[data-cache-ajax]"),
-        shouldMakeAjaxCall = (ajaxIsntCached || !ajaxIsCacheable);
+      href      = button.attr("href"),
+      element   = button.data('popupElement'),
+      isAjax    = (href[0] !== "#"),
+      ajaxIsntCached     = !element,
+      ajaxIsCacheable    = button.is("[data-cache-ajax]"),
+      shouldMakeAjaxCall = (ajaxIsntCached || !ajaxIsCacheable);
 
     if(! href){
       throw("[actsAsPopup]: href is empty");
     }
 
     if(isAjax && shouldMakeAjaxCall ){
-      //there is no ajax server in testing
-      //$.post(href, {html: "<p>test</p>"}, function(data){
-      //  postHandler(data, button);
-      //});
-      postHandler("jakhdfjkads", button);
+      $.get(href, function(data){
+        postHandler(data, button);
+      });
     } else {
       var popup = element || $(href);
       togglePopup(popup, button);
@@ -56,6 +57,26 @@
 
   $.fn.exoPopup = function(){
     this.live('click', actsAsPopup);
+
+  };
+
+
+  $.fn.exoPositionRelative = function(child, opts){
+    var parent   = this,
+      options    = opts || {},
+      offsetTop  = options.top  || 0,
+      offsetLeft = options.left || 0;
+
+
+    $(window).resize( function(e){
+      var offset = parent.offset(),
+        top    = offset.top + offsetTop,
+        left   = offset.left + offsetLeft;
+
+      child.css({left: left, top: top });
+    });
+
+    $(window).trigger('resize');
   };
 
 })(jQuery);
